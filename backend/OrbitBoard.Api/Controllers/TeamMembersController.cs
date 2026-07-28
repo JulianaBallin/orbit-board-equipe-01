90d19e87da87a7d0be60c698f9d80a5570de7d73
cawzkf <camilafelixdosreis@gmail.com>
2026-07-28 01:07:35 -0400

    feat(backend): add the endpoint to register a team member

--- backend/OrbitBoard.Api/Controllers/TeamMembersController.cs
+++ backend/OrbitBoard.Api/Controllers/TeamMembersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using OrbitBoard.Api.DTOs;
 using OrbitBoard.Api.Models;
 using OrbitBoard.Api.Services;
 
@@ -11,4 +12,14 @@ public sealed class TeamMembersController(IWorkspaceService service) : Controlle
     [HttpGet]
     [ProducesResponseType<IReadOnlyList<TeamMember>>(StatusCodes.Status200OK)]
     public ActionResult<IReadOnlyList<TeamMember>> GetAll() => Ok(service.GetTeamMembers());
+
+    [HttpPost]
+    [ProducesResponseType<TeamMember>(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public ActionResult<TeamMember> Create(CreateTeamMemberRequest request)
+    {
+        var member = service.CreateTeamMember(request);
+        return CreatedAtAction(nameof(GetAll), new { id = member.Id }, member);
+    }
 }
